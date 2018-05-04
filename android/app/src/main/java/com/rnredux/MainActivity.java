8bccc68d989788009becf53ee6bb01e148f5b549
陈宇 <535509852@qq.com>
2018-05-04 19:25:34 +0800

    添加启动屏

--- android/app/src/main/java/com/rnredux/MainActivity.java
+++ android/app/src/main/java/com/rnredux/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rnredux;
 
+import android.os.Bundle; // here 
 import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1 
+import org.devio.rn.splashscreen.SplashScreen; // here 
+// react-native-splash-screen < 0.3.1 
+// import com.cboy.rn.splashscreen.SplashScreen; // here 
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +16,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "rnRedux";
+    }
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            SplashScreen.show(this);  // here 
+            super.onCreate(savedInstanceState);
     }
 }
--- android/app/src/main/java/com/rnredux/MainApplication.java
+++ android/app/src/main/java/com/rnredux/MainApplication.java
@@ -8,6 +8,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+// react-native-splash-screen >= 0.3.1 
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
+// react-native-splash-screen < 0.3.1 
+// import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SplashScreenReactPackage()
       );
     }
 
